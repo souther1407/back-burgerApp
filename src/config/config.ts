8ccb32b0a65006c673b4858370e7c6f0f0be34e6
Eric Casanova <kilbane1407@gmail.com>
2022-11-11 15:52:50 -0300

    deploy nueva nube

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-const MOCK_HEROKU_DB_URL =
-  "postgres://iwwrgcyvmevedt:848876c9e0c2bfc9b0c74fccab597a1c60345934519ed959d973f39066c24b22@ec2-35-170-21-76.compute-1.amazonaws.com:5432/d9uphu91j07qe2";
 const config = {
   TOKEN_SECRET: process.env.TOKEN_SECRET,
-  DATABASE_URL: process.env.DATABASE_URL || MOCK_HEROKU_DB_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   PASSWORD_SECRET: process.env.PASSWORD_SECRET,
   USER_TOKEN_SECRET: process.env.USER_TOKEN_SECRET,
   PORT: process.env.PORT || 8080,
--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import config from "../config/config.js";
-const db = new Sequelize(config.DATABASE_URL, {
+const db = new Sequelize(config.DATABASE_URL!, {
   dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
 
