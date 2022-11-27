93a96465023e559aec3f447ed4d897d25af0b57c
Eric Casanova <kilbane1407@gmail.com>
2022-11-27 18:59:42 -0300

    controlador de sliders agregado

--- src/Sliders/controllers/sliders.controllers.ts
+++ src/Sliders/controllers/sliders.controllers.ts
@@ -4,10 +4,49 @@ import {
   modificarSlider,
   obtenerSliders,
 } from "../services/sliders.services.js";
-
 import { Request, Response } from "express";
+import { OK } from "../../httpCodes.js";
+import ErrorHandler from "../../utils/ErrorHandler.js";
+const { handleHttpError } = ErrorHandler;
 
-const obtenerTodosLosSliders = (req: Request, res: Response) => {
+const obtenerTodosLosSliders = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const { token } = req.body;
+    const sliders = await obtenerSliders(token);
+    res.status(OK).json(sliders);
+  } catch (error) {
+    handleHttpError(error as Error, res);
+  }
+};
+
+const crearUnSlider = async (req: Request, res: Response) => {
+  try {
+    const user = (req as any).user;
+    const slider = await crearSlider(req.body, user.id);
+    res.status(OK).json(slider);
+  } catch (error) {
+    handleHttpError(error as Error, res);
+  }
+};
+
+const modificarUnSlider = async (req: Request, res: Response) => {
+  try {
+    const user = (req as any).user;
+    const { id } = req.params;
+    const slider = await modificarSlider(id, req.body, user.id);
+    res.status(OK).json(slider);
+  } catch (error) {
+    handleHttpError(error as Error, res);
+  }
+};
+
+const borrarUnSlider = async (req: Request, res: Response) => {
+  try {
+    const user = (req as any).user;
+    const { id } = req.params;
+    const borrado = await borrarSlider(id, user.id);
+    res.status(OK).json(borrado);
+  } catch (error) {
+    handleHttpError(error as Error, res);
+  }
 };
